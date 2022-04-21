79d9d722a193ae9b62c0ef2628237943c7bcc456
8a6_1176022890_weitao <8a6_1176022890_weitao@git.cloud.tencent.com>
2022-04-21 19:52:18 +0800

    docker

---
+++ dist/static/js/notice-f237c64b.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-1dd190b0.js";const e=e=>t({url:"notice",method:"get",params:e}),o=e=>t({url:"notice",method:"post",data:e}),a=(e,o)=>t({url:`notice/${e}`,method:"put",data:o}),d=e=>t({url:`notice/${e}`,method:"delete"}),s=e=>t({url:`notice/${e}`,method:"get"});export{d as a,s as b,a as c,o as d,e as n};
