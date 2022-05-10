67ec0ed5f25980a8aa96c59f2a482d218eba5053
Wei.Tao <extern.tao.wei@mosi-tech.com>
2022-05-10 21:55:23 +0800

    dist

--- dist/static/js/resources-eb5bbfc1.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-2f4a0259.js";const s=s=>e({url:"resources",method:"get",params:s}),r=s=>e({url:"resources",method:"post",data:s}),a=(s,r)=>e({url:`resources/${s}`,method:"patch",data:r}),o=s=>e({url:`resources/${s}`,method:"delete"});export{o as a,a as b,r as c,s as r};
